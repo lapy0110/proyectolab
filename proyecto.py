860bf5451fb6e9ff0afae49b51081c904ccf8172
Pino <felix@hotmail.com>
2018-06-20 12:17:34 -0400

    Lo mismo pero con un If

--- proyecto.py
+++ proyecto.py
@@ -5,12 +5,18 @@ from pygame import *
 #Veclaración Variables
 e=0
 #Funciones
+
 #Main
 print("Bienvenido al compositor musical")
 print("Seleccione la opción que desea realizar:")
 print("	1-.Cargar Composición")
 print("	2-.Escuchar composición")
 print("	3-. Salir")
-e
+
+e=int(input("Inserte la opión: "))
+
+if (e==1):
+
+
 
 
