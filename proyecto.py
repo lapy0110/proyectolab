cee1be1ef99774c944afc039033601096d1dbc99
felix <felix@hotmail.com>
2018-06-20 12:00:17 -0400

    Agregado el primer prototipo de menú

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,16 @@
- 
-print("Proyecto")
-print("Hola Mundo")
-print("Compositor Musical")
-print("Esta es la rama de felix")
+#Librerias
+import sys
+from music21 import *
+from pygame import *
+#Veclaración Variables
+e=0
+#Funciones
+#Main
+print("Bienvenido al compositor musical")
+print("Seleccione la opción que desea realizar:")
+print("	1-.Cargar Composición")
+print("	2-.Escuchar composición")
+print("	3-. Salir")
+e
+
+
