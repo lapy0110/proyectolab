cd5fd2ca9ed25f4d898feac414d80795738c76c5
Pino <felix@hotmail.com>
2018-06-21 19:16:48 -0400

    Agregado lso submenus 1 y 2, implementación de menu

--- proyecto.py
+++ proyecto.py
@@ -1,21 +1,45 @@
-#Librerias
+#############################################Librerias
+
 import sys
 from music21 import *
 from pygame import *
-#Veclaración Variables
-e=0
-#Funciones
 
-#Main
+########################################Veclaración Variables####################################3
+
+e=0
+
+######################################################Funciones##########################################
+
+def MenuSelect (x) -> 'void':
+	if (x==1):
+		MenuComposición(x)
+	elif(x==2):
+		sys.exit()
+
+def MenuComposición (x) -> 'void':
+	print("---------------------------------------------")
+	print("1-. Registrar Parte 1")
+	print("2-. Registrar Parte 2")
+	print("3-. Registrar Parte 3")
+	print("4-. Registrar Parte 4")
+	print("5-. Escuchar Composición")
+	print("6-. Salir")
+	y=int(input("Introduzca la opción que considere: "))
+
+	if (y==6):
+		sys.exit()
+
+
+##########################################################Main#####################################################
 print("Bienvenido al compositor musical")
 print("Seleccione la opción que desea realizar:")
 print("	1-.Cargar Composición")
-print("	2-.Escuchar composición")
-print("	3-. Salir")
+print("	2-.Salir")
 
-e=int(input("Inserte la opión: "))
+e=int(input("Inserte la opicón que considere: "))
+MenuSelect(e)
 
-if (e==1):
+
 
 
 
